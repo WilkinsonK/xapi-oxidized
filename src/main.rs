530fcdb7ee77b905ebd908014134632eb604e996
WilkinsonK <keenanwilkinson@outlook.com>
2024-01-05 07:17:24 -0600

    update testable example to reflect new API changes

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,10 @@ use tokio;
 async fn main() -> Result<(), Error> {
     // Pass a base URL as the host name in this
     // method call.
-    let session = oxr::Session::from_host("");
+    let mut session = oxr::Session::from_host("");
     // Build and send a new request using `surf`
     // API.
-    let mut req = session.get("xapi/users/username")?.await?;
+    let mut req = session.get()?.await?;
     // Attempt to parse the response body.
     println!("from body: [{}]({})", req.body_string().await?, req.status());
     Ok(())
