131bd633eb0f61aa897779b9f11d4209900ea765
WilkinsonK <keenanwilkinson@outlook.com>
2024-05-19 15:26:20 -0500

    add archive uri builders

--- xnat_core/src/uri_sys.rs
+++ xnat_core/src/uri_sys.rs
@@ -67,12 +67,73 @@ where
     }
 }
 
+/// Represents the URI paths available for
+/// endpoints to download the specified catalog.
+#[derive(Clone, Debug, Default, UriBuilder)]
+#[match_path(path = "{parent}/download")]
+#[match_path(path = "{parent}/download/{catalog_id}")]
+pub struct DownloadUriBuilder<Parent>
+where
+    Parent: SysUriBuilder,
+{
+    #[param]
+    catalog_id: Option<String>,
+    #[parent]
+    parent: Option<Rc<Parent>>
+}
+
+impl<Parent> DownloadUriBuilder<ArchiveUriBuilder<Parent>>
+where
+    Parent: SysUriBuilder,
+{
+    /// Produce the
+    /// archive/download/{catalog_id}/test URI
+    /// endpoint.
+    pub fn build_test(&self) -> anyhow::Result<String> {
+        self.build_join("test")
+    }
+
+    /// Produce the archive/downloadwithsize URI
+    /// endpoint.
+    pub fn build_with_size(&self) -> anyhow::Result<String> {
+        self.parent.as_ref().unwrap().build_join("downloadwithsize")
+    }
+
+    /// Produce the
+    /// archive/download/{catalog_id}/xml URI
+    /// endpoint.
+    pub fn build_xml(&self) -> anyhow::Result<String> {
+        self.build_join("xml")
+    }
+
+    /// Produce the
+    /// archive/download/{catalog_id}/zip URI
+    /// endpoint.
+    pub fn build_zip(&self) -> anyhow::Result<String> {
+        self.build_join("zip")
+    }
+}
+
 impl<Parent> ArchiveUriBuilder<Parent>
 where
     Parent: SysUriBuilder + Default,
 {
+    /// Continue the builder into a
+    /// `CatalogsUriBuilder`.
     pub fn catalog(&self) -> CatalogsUriBuilder<Self> {
         CatalogsUriBuilder::from_parent(self.clone().into())
+    }
+
+    /// Continue the builder into a
+    /// `DownloadUriBuilder`.
+    pub fn download(&self) -> DownloadUriBuilder<Self> {
+        DownloadUriBuilder::from_parent(self.clone().into())
+    }
+
+    /// Produce the archive/upload/xml URI
+    /// endpoint.
+    pub fn build_upload_xml(&self) -> anyhow::Result<String> {
+        self.build_join("upload/xml")
     }
 }
 
