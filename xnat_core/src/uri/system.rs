00614d801631d61d6b061e28215dbe9eea5302ee
WilkinsonK <keenanwilkinson@outlook.com>
2024-05-21 15:41:29 -0500

    finalize URI support for system administration

--- xnat_core/src/uri/system.rs
+++ xnat_core/src/uri/system.rs
@@ -9,12 +9,17 @@ ImplSysUriBuilder! {
     (String),
 }
 ImplSysUriBuilder! {
+    (AllowUriBuilder<Parent>, Parent),
     (ArchiveUriBuilder<Parent>, Parent),
-    (RefreshUriBuilder<Parent>, Parent),
+    (AsyncOpsUriBuilder<Parent>, Parent),
     (CatalogsUriBuilder<Parent>, Parent),
     (DownloadUriBuilder<Parent>, Parent),
+    (MessagesUriBuilder<Parent>, Parent),
     (NotificationsUriBuilder<Parent>, Parent),
     (NotifyUriBuilder<Parent>, Parent),
+    (RefreshUriBuilder<Parent>, Parent),
+    (SubscribersUriBuilder<Parent>, Parent),
+    (XnatTaskUriBuilder<Parent>, Parent),
 }
 
 /// Represents the URI paths available for
@@ -153,6 +158,48 @@ where
     parent: Option<Rc<Parent>>
 }
 
+/// Represents URI endpoint paths available for
+/// managing some allowables for XNAT
+/// notifications.
+#[derive(Clone, Debug, Default, UriBuilder)]
+#[match_path(path = "{parent}/allow/{name}")]
+#[match_path(path = "{parent}/allow/{name}/{setting}")]
+pub struct AllowUriBuilder<Parent>
+where
+    Parent: SysUriBuilder,
+{
+    #[param]
+    name: Option<String>,
+    #[param]
+    setting: Option<String>,
+    #[parent]
+    parent: Option<Rc<Parent>>,
+}
+
+/// Some supported message type by an XNAT
+/// notification system.
+#[derive(Clone, Debug, UriBuilder)]
+pub enum MessageType {
+    ForgotUserName,
+    Help,
+    PasswordReset,
+    Registration,
+}
+
+/// Represents URI endpoint paths available for
+/// user messaging configuration.
+#[derive(Clone, Debug, Default, UriBuilder)]
+#[match_path(path = "{parent}/messages/{message_type}")]
+pub struct MessagesUriBuilder<Parent>
+where
+    Parent: SysUriBuilder,
+{
+    #[param]
+    message_type: Option<MessageType>,
+    #[parent]
+    parent: Option<Rc<Parent>>,
+}
+
 /// Some supported notification type by an XNAT
 /// notification system.
 #[derive(Clone, Debug, UriBuilder)]
@@ -188,16 +235,132 @@ where
     #[param]
     notify_type: Option<NotifyType>,
     #[parent]
-    parent: Option<Rc<Parent>>
+    parent: Option<Rc<Parent>>,
+}
+
+/// Some supported options available for managing
+/// subscriber config options.
+#[derive(Clone, Debug, UriBuilder)]
+pub enum SubscriberOption {
+    Error,
+    Issue,
+    NewUser,
+    Update,
+}
+
+/// Represents URI endpoint paths available for
+/// managing subscriber notification
+/// configuration.
+#[derive(Clone, Debug, Default, UriBuilder)]
+#[match_path(path = "{parent}/subscribers/{subscriber_option}")]
+pub struct SubscribersUriBuilder<Parent>
+where
+    Parent: SysUriBuilder,
+{
+    #[param]
+    subscriber_option: Option<SubscriberOption>,
+    #[parent]
+    parent: Option<Rc<Parent>>,
 }
 
 impl<Parent> NotificationsUriBuilder<Parent>
 where
     Parent: SysUriBuilder + Default,
 {
+    /// Continue the builder into a
+    /// `AllowUriBuilder`.
+    pub fn allow(&self) -> AllowUriBuilder<Self> {
+        AllowUriBuilder::from_parent(self.clone().into())
+    }
+
+    /// Continue the builder into a
+    /// `MessagesUriBuilder`.
+    pub fn messages(&self) -> MessagesUriBuilder<Self> {
+        MessagesUriBuilder::from_parent(self.clone().into())
+    }
+
+    /// Continue the builder into a
+    /// `NotifyUriBuilder`.
     pub fn notify(&self) -> NotifyUriBuilder<Self> {
         NotifyUriBuilder::from_parent(self.clone().into())
     }
+
+    /// Continue the builder into a
+    /// `SubscribersUriBuilder`.
+    pub fn subscribers(&self) -> SubscribersUriBuilder<Self> {
+        SubscribersUriBuilder::from_parent(self.clone().into())
+    }
+}
+
+/// Represents the URI paths available for
+/// managing XNAT tasks.
+#[derive(Clone, Debug, Default, UriBuilder)]
+#[match_path(path = "{parent}/xnatTask")]
+pub struct XnatTaskUriBuilder<Parent>
+where
+    Parent: SysUriBuilder,
+{
+    #[parent]
+    parent: Option<Rc<Parent>>,
+}
+
+impl<Parent> XnatTaskUriBuilder<Parent>
+where
+    Parent: SysUriBuilder,
+{
+    /// Produce the
+    /// archive/xnatTask/checkNodeConfigurationStatus
+    /// URI endpoint.
+    pub fn check_node_config_status(&self) -> anyhow::Result<String> {
+        self.build_join("checkNodeConfigurationStatus")
+    }
+
+    /// Produce the archive/xnatTask/taskList URI
+    /// endpoint.
+    pub fn task_list(&self) -> anyhow::Result<String> {
+        self.build_join("taskList")
+    }
+}
+
+/// Represents the URI paths available for
+/// managing XNAT asyncronous operations.
+#[derive(Clone, Debug, Default, UriBuilder)]
+#[match_path(path = "{parent}/asyncOps")]
+#[match_path(path = "{parent}/asyncOps/{preference}")]
+pub struct AsyncOpsUriBuilder<Parent>
+where
+    Parent: SysUriBuilder,
+{
+    #[param]
+    preference: Option<String>,
+    #[parent]
+    parent: Option<Rc<Parent>>,
+}
+
+#[derive(Clone, Debug, UriBuilder)]
+pub enum LogConfigOpt {
+    #[match_path(path = "configs")]
+    #[match_path(path = "configs/{p0}")]
+    Configs(Option<String>),
+    #[match_path(path = "download")]
+    #[match_path(path = "download/{p0}")]
+    Download(Option<String>),
+    Elements,
+    Reset,
+}
+
+/// Represents the URI paths available for
+/// managing XNAT asyncronous operations.
+#[derive(Clone, Debug, Default, UriBuilder)]
+#[match_path(path = "{parent}/logs/{config_opt}")]
+pub struct LogsUriBuilder<Parent>
+where
+    Parent: SysUriBuilder,
+{
+    #[param]
+    config_opt: Option<LogConfigOpt>,
+    #[parent]
+    parent: Option<Rc<Parent>>,
 }
 
 /// Represent the URI paths available for
@@ -210,11 +373,32 @@ pub trait SystemUri: Version {
     fn archive(&self) -> ArchiveUriBuilder<String> {
         ArchiveUriBuilder::from_parent(self.root_uri().into())
     }
-         
+
+    /// URI endpoint to interact with XNAT async
+    /// ops API.
+    #[inline]
+    fn async_ops(&self) -> AsyncOpsUriBuilder<String> {
+        AsyncOpsUriBuilder::from_parent(self.root_uri().into())
+    }
+
+    /// URI endpoint to interact with XNAT log
+    /// configurations.
+    #[inline]
+    fn logs(&self) -> LogsUriBuilder<String> {
+        LogsUriBuilder::from_parent(self.root_uri().into())
+    }
+
     /// URI endpoint to interact with XNAT
     /// notifications API.
     #[inline]
     fn notifications(&self) -> NotificationsUriBuilder<String> {
         NotificationsUriBuilder::from_parent(self.root_uri().into())
+    }
+
+    /// URI enpoint to interact with XNAT task
+    /// API.
+    #[inline]
+    fn xnat_task(&self) -> XnatTaskUriBuilder<String> {
+        XnatTaskUriBuilder::from_parent(self.root_uri().into())
     }
 }
