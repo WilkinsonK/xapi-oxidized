1d29168a3e889983571c62d0ea7fc5bc9b6a59a0
WilkinsonK <keenanwilkinson@outlook.com>
2024-06-12 11:08:57 -0500

    rework experiment retrieval to allow for varying paths

--- xnat/tests/protocols_test.rs
+++ xnat/tests/protocols_test.rs
@@ -1,21 +1,37 @@
 mod common;
 
 use oxinat::{
-    models::{Project, Subject},
-    protocols::Retrieve
+    models::{Experiment, Project, Subject},
+    protocols::Retrieve, ClientToken
 };
 use oxinat_core::anyhow;
+
+#[tokio::test]
+#[ignore = "must have a .env file for variables set in env"]
+async fn test_protocols_experiment_retreive01() {
+    common::init();
+
+    let mut client = common::request_client().await;
+
+    let found: anyhow::Result<Vec<Experiment>> = client.get_all().await;
+    assert!(found.is_ok(), "must be able to retrieve `Experiment`s from host: {found:?}");
+    assert!(!found.unwrap().is_empty(), "results must have `some` items");
+
+    client.release().await.unwrap();
+}
 
 #[tokio::test]
 #[ignore = "must have a .env file for variables set in env"]
 async fn test_protocols_project_retrieve01() {
     common::init();
 
-    let client = common::request_client().await;
+    let mut client = common::request_client().await;
 
     let found: anyhow::Result<Vec<Project>> = client.get_all().await;
     assert!(found.is_ok(), "must be able to retrieve `Project`s from host: {found:?}");
-    assert!(!found.unwrap().is_empty(), "results must have `some` items")
+    assert!(!found.unwrap().is_empty(), "results must have `some` items");
+
+    client.release().await.unwrap();
 }
 
 #[tokio::test]
@@ -23,13 +39,15 @@ async fn test_protocols_project_retrieve01() {
 async fn test_protocols_project_retrieve02() {
     common::init();
 
-    let client = common::request_client().await;
+    let mut client = common::request_client().await;
     let mut model = Project::default();
     model.id.clone_from(&common::env_project_id().into());
 
     let found = client.get_any_from(&model).await;
     assert!(found.is_ok(), "must be able to retrieve `Projects`s from host: {found:?}");
     assert!(!found.unwrap().is_empty(), "should contain at least one project");
+
+    client.release().await.unwrap();
 }
 
 #[tokio::test]
@@ -37,11 +55,13 @@ async fn test_protocols_project_retrieve02() {
 async fn test_protocols_subject_retrieve01() {
     common::init();
 
-    let client = common::request_client().await;
+    let mut client = common::request_client().await;
 
     let found: anyhow::Result<Vec<Subject>> = client.get_all().await;
     assert!(found.is_ok(), "must be able to retrieve `Subject`s from host: {found:?}");
-    assert!(!found.unwrap().is_empty(), "results must have `some` items")
+    assert!(!found.unwrap().is_empty(), "results must have `some` items");
+
+    client.release().await.unwrap();
 }
 
 #[tokio::test]
@@ -49,12 +69,14 @@ async fn test_protocols_subject_retrieve01() {
 async fn test_protocols_subject_retrieve02() {
     common::init();
 
-    let client = common::request_client().await;
+    let mut client = common::request_client().await;
     let model = Subject::default();
 
     let found = client.get_any_from(&model).await;
     assert!(found.is_ok(), "must be able to retrieve `Subject`s from host: {found:?}");
     assert!(!found.unwrap().is_empty(), "should contain at least one subject");
+
+    client.release().await.unwrap();
 }
 
 #[tokio::test]
@@ -62,11 +84,13 @@ async fn test_protocols_subject_retrieve02() {
 async fn test_protocols_subject_retrieve03() {
     common::init();
 
-    let client = common::request_client().await;
+    let mut client = common::request_client().await;
     let mut model = Subject::default();
     model.project.clone_from(&common::env_project_id().into());
 
     let found = client.get_any_from(&model).await;
     assert!(found.is_ok(), "must be able to retrieve `Subject`s from host: {found:?}");
     assert!(!found.unwrap().is_empty(), "should contain at least one subject");
+
+    client.release().await.unwrap();
 }
--- xnat_core/src/protocols/retrieve.rs
+++ xnat_core/src/protocols/retrieve.rs
@@ -15,6 +15,27 @@ use crate::models::{
 use crate::version::Version;
 use super::crud::Retrieve;
 
+macro_rules! retrieve_rst_vec {
+    ($client:ident, $uri:ident, $model:ident) => {
+        $client
+            .get_any_result_from(&$uri, &$model)
+            .await?
+            .results()
+            .to_vec()
+    };
+}
+
+macro_rules! retreive_its_vec {
+    ($client:ident, $uri: ident, $model:ident) => {
+        $client
+            .get_any_items_from(&$uri, &$model)
+            .await?
+            .iter()
+            .map(|i| i.unwrap())
+            .collect::<Vec<_>>()
+    }
+}
+
 #[async_trait(?Send)]
 impl<V> Retrieve<Project> for Xnat<V>
 where
@@ -30,18 +51,9 @@ where
             Some(i) => {
                 uri = uri.with_id(i);
                 model_clone.id = None;
-                self
-                    .get_any_items_from(&uri, &model_clone)
-                    .await?
-                    .iter()
-                    .map(|i| i.unwrap())
-                    .collect::<Vec<_>>()
+                retreive_its_vec!(self, uri, model_clone)
             },
-            None => self
-                .get_any_result_from(&uri, &model_clone)
-                .await?
-                .results()
-                .to_vec()
+            None => retrieve_rst_vec!(self, uri, model_clone)
         })
     }
 }
@@ -76,24 +88,11 @@ where
 
         let data = if let Subject { project: Some(p), .. } = &model_clone {
             let uri = uri.by_project(p);
-            self
-                .get_any_result_from(&uri, &model_clone)
-                .await?
-                .results()
-                .to_vec()
+            retrieve_rst_vec!(self, uri, model_clone)
         } else if get_as_item {
-            self
-                .get_any_items_from(&uri, &model_clone)
-                .await?
-                .iter()
-                .map(|i| i.unwrap())
-                .collect::<Vec<_>>()
+            retreive_its_vec!(self, uri, model_clone)
         } else {
-            self
-                .get_any_result_from(&uri, &model_clone)
-                .await?
-                .results()
-                .to_vec()
+            retrieve_rst_vec!(self, uri, model_clone)
         };
         Ok(data)
     }
@@ -103,28 +102,117 @@ where
 impl<V> Retrieve<Experiment> for Xnat<V>
 where
     Self: ClientCore<Version = V> + ClientREST,
-    V: Version + ExperimentUri,
+    V: Version + ProjectUriLegacy + SubjectUriLegacy + ExperimentUri,
 {
     async fn get_any_from(&self, model: &Experiment) -> anyhow::Result<Vec<Experiment>> {
-        let mut uri = self.version().experiment_data();
         let mut model_clone = model.clone();
 
+        // Filter over model values that are only
+        // useful as URI params.
+        let experiment = &model_clone
+            .id
+            .clone()
+            .or(model_clone.label);
+
+        let subject = &model_clone
+            .subject_id
+            .clone()
+            .or(model_clone.subject_label);
+
+        let project = &model_clone
+            .project
+            .clone()
+            .or(model_clone.subject_project);
+
+        // Clear out identifiers to avoid
+        // polluting query params.
+        model_clone.project         = None;
+        model_clone.subject_project = None;
+        model_clone.id              = None;
+        model_clone.label           = None;
+        model_clone.subject_id      = None;
+        model_clone.subject_label   = None;
+        // Set returning format to JSON.
         model_clone.format = Some(FormatSpecifier::Json);
-        Ok(match &model.id {
-            Some(_) => {
-                uri = uri.with_experiment(model_clone.id.take().unwrap());
-                self
-                    .get_any_items_from(&uri, &model_clone)
-                    .await?
-                    .iter()
-                    .map(|i| i.unwrap())
-                    .collect::<Vec<_>>()
-            },
-            None => self
-                .get_any_result_from(&uri, &model_clone)
-                .await?
-                .results()
-                .to_vec()
-        })
+
+        // When specifying the experiment, we are
+        // expecting an item response.
+        let data = if experiment.is_some() {
+            let experiment = experiment.clone().unwrap();
+            match [subject, project] {
+                [Some(s), Some(p)] => {
+                    let uri = self
+                        .version()
+                        .project_data()
+                        .with_id(p)
+                        .subjects()
+                        .with_subject(s)
+                        .experiments()
+                        .with_experiment(experiment);
+                    retreive_its_vec!(self, uri, model_clone)
+                },
+                [None, Some(p)] => {
+                    let uri = self
+                        .version()
+                        .project_data()
+                        .with_id(p)
+                        .experiments()
+                        .with_experiment(experiment);
+                    retreive_its_vec!(self, uri, model_clone)
+                },
+                [Some(s), None] => {
+                    let uri = self
+                        .version()
+                        .subject_data()
+                        .with_subject(s)
+                        .experiments()
+                        .with_experiment(experiment);
+                    retreive_its_vec!(self, uri, model_clone)
+                },
+                [None, None] => {
+                    let uri = self
+                        .version()
+                        .experiment_data()
+                        .with_experiment(experiment);
+                    retreive_its_vec!(self, uri, model_clone)
+                },
+            }
+        } else {
+            match [subject, project] {
+                [Some(s), Some(p)] => {
+                    let uri = self
+                        .version()
+                        .project_data()
+                        .with_id(p)
+                        .subjects()
+                        .with_subject(s)
+                        .experiments();
+                    retrieve_rst_vec!(self, uri, model_clone)
+                },
+                [None, Some(p)] => {
+                    let uri = self
+                        .version()
+                        .project_data()
+                        .with_id(p)
+                        .experiments();
+                    retrieve_rst_vec!(self, uri, model_clone)
+                },
+                [Some(s), None] => {
+                    let uri = self
+                        .version()
+                        .subject_data()
+                        .with_subject(s)
+                        .experiments();
+                    retrieve_rst_vec!(self, uri, model_clone)
+                },
+                [None, None] => {
+                    let uri = self
+                        .version()
+                        .experiment_data();
+                    retrieve_rst_vec!(self, uri, model_clone)
+                },
+            }
+        };
+        Ok(data)
     }
 }
